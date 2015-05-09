af038eb9c9f26697c1e042940f39967b516ecf60
Alexi Maschas <alexi.maschas@gmail.com>
2015-05-09 01:46:09 -0400

    adding initial files

---
+++ README.md
@@ -0,0 +1,24 @@
+#jQuery-Breakpoints.js
+
+**Simple breakpoint events for responsive JS.**
+
+##Define your breakpoints (*desktop is set by default*):
+
+```
+$( window ).breakpoints( {
+	breakpoints : { 
+		'mobile-small' : 320,
+		'mobile' : 640,
+		'tablet' : 1024
+	}
+} );
+```
+
+##Subscribe to a breakpoint event:
+
+```
+$( window ).on( 'breakpointChange', function( e, data ) {
+	console.log( 'Active Breakpoint: ' + data.activeBreakpoint );
+	console.log( 'Deactivated Breakpoint: ' + data.deactivatedBreakpoint );
+} );
+```
---
+++ jquery-breakpoints.js
@@ -0,0 +1,64 @@
+
+n( $ ) {
+
+	$.fn.breakpoints = function( args ) {
+
+		// Initialize defaults and settings
+		var defaults = {
+			breakpoints : {
+				'desktop' : -1
+			}
+		},
+		settings = $.extend( true, {}, defaults, args ),
+		currentBreakpoint;
+
+		// Sort the breakpoints array
+		settings.breakpoints = $.map( settings.breakpoints, function( breakpointWidth, breakpointID ){
+			return { breakpointID, breakpointWidth };
+		} ).sort( function( a, b ) {
+			return a.breakpointWidth - b.breakpointWidth;
+		} );
+
+		// Initial breakpoint on document ready
+		$( document ).ready( function() {
+			getBreakpoint();
+		} );
+		
+		// Check breakpoint on window resize
+		$( window ).resize( function() {
+			getBreakpoint();
+		} );
+
+		// Checks the breakpoints and dispatches events
+		function getBreakpoint() {
+			var floatingBreakpoint = {
+				'breakpointID': 'desktop',
+				'breakpointWidth': settings.breakpoints[0].breakpointWidth
+			};
+
+			// Compare sorted breakpoint list
+			$.each( settings.breakpoints, function( index, breakpoint ) {
+				var width = $( window ).width();
+
+				// Break on first breakpoint larger than window width
+				if ( breakpoint.breakpointWidth >= width ) {
+					floatingBreakpoint.breakpointWidth = breakpoint.breakpointWidth;
+					floatingBreakpoint.breakpointID = breakpoint.breakpointID;
+					return false;
+				}
+			} );
+
+			// Check if the breakpoint has changed
+			if ( currentBreakpoint === undefined || currentBreakpoint.breakpointWidth !== floatingBreakpoint.breakpointWidth ) {
+				var activeBreakpointID = floatingBreakpoint.breakpointID,
+				deactivatedBreakpointID = currentBreakpoint === undefined ? '' : currentBreakpoint.breakpointID;
+				currentBreakpoint = floatingBreakpoint;
+
+				// Dispatch breakpoint changed event
+				$( window ).trigger( 'breakpointChange', { 'activeBreakpoint' : activeBreakpointID, 'deactivatedBreakpoint' : deactivatedBreakpointID } );
+			}
+
+		}
+	}
+
+} )( jQuery );
